dbdccf1812defb96594dc272489acc31cb544e2c
dyyao <646617513@qq.com>
2018-04-02 18:15:41 +0800

    添加上传模块

---
+++ src/components/upload/upload.vue
@@ -0,0 +1,16 @@
+<template>
+    <label id="container" class="container">
+      <input type="file" id="button" class="button">
+    </label>
+</template>
+
+<script>
+    export default {
+        name: "upload"
+    }
+</script>
+
+<style scoped>
+
+
+</style>
--- src/views/index/index.vue
+++ src/views/index/index.vue
@@ -1,15 +1,42 @@
 <template>
-    <div>
-      我是首页
-    </div>
+  <div class="videoUp">
+    <el-form :model="formData" label-width="80px">
+      <el-form-item label="视频标题">
+        <el-input v-model="formData.title"></el-input>
+      </el-form-item>
+      <el-form-item label="视频头图">
+        <el-input v-model="formData.img"></el-input>
+      </el-form-item>
+      <el-form-item label="视频">
+        <upload></upload>
+      </el-form-item>
+    </el-form>
+  </div>
 </template>
 
 <script>
-    export default {
-        name: "index"
+  import upload from '../../components/upload'
+  export default {
+    name: "index",
+    components: {
+      upload
+    },
+    data() {
+      return {
+        formData: {
+          title: '',
+          video: '',
+          img: ''
+        }
+      }
     }
+  }
 </script>
 
 <style scoped>
+  .videoUp {
+    padding-top: 20px;
+    padding-right: 20px;
+  }
 
 </style>
